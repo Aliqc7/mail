fba12811b587139386637728d95f0cdf737c7d3b
Ali <ali.zolghadr@gmail.com>
2024-01-20 18:21:32 +1100

    Add view single email functionality

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
   
   // Send mail when compose-from is submitted
-  document.querySelector("#compose-form").addEventListener('submit', () => send_mail())
+  document.querySelector("#compose-form").addEventListener('submit', (event) => send_mail(event))
   
   
   // By default, load the inbox
@@ -18,6 +18,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single_email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -31,6 +32,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single_email-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -46,20 +48,25 @@ function load_mailbox(mailbox) {
             div_element.style.margin = '2px'
             div_element.style.textAlign = 'center'
             if (email.read) {
-                div_element.style.background = 'grey'
+                div_element.style.background = '#CDCDCD'
             } else {
                 div_element.style.background = 'white'
             }
             div_element.innerHTML = `Sender: ${email.sender} ---- Subject: ${email.subject}, ---- Time: ${email.timestamp}`;
-            emailsView.append(div_element);
-            console.log(email)
+            
+            const a_element = document.createElement('a');
+            a_element.href = '#';
+            a_element.addEventListener('click', (event) => view_mail(event));
+            div_element.id = email.id;
+            a_element.appendChild(div_element);
+            emailsView.appendChild(a_element);
         })
     });
 
 }
 
-function send_mail(){
-    this.event.preventDefault()
+function send_mail(event){
+    event.preventDefault()
     const recipients = document.querySelector("#compose-recipients").value;
     const subject = document.querySelector("#compose-subject").value;
     const body = document.querySelector("#compose-body").value;
@@ -77,4 +84,34 @@ function send_mail(){
         console.log(result);
     });
     load_mailbox('sent');
+}
+
+function view_mail(event){
+    event.preventDefault();
+    document.querySelector('#single_email-view').style.display = 'block';
+    email_id = event.target.id
+
+    fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    .then(email => {
+        const mail_div_element = document.querySelector("#single_email-view");
+        const sender_el = document.createElement('h4');
+        sender_el.textContent = `Sender: ${email.sender}`;
+        const recipients_el = document.createElement('h4');
+        recipients_el.textContent = `Recipients: ${email.recipients}`;
+        const subject_el = document.createElement('h4')
+        subject_el.textContent = `Subject: ${email.subject}`;
+        const timestamp_el = document.createElement('p');
+        timestamp_el.textContent = `Time: ${email.timestamp}`;
+        const body_el = document.createElement('p');
+        body_el.setAttribute('style', 'white-space: pre;');
+        body_el.textContent = "Content: \r\n";
+        body_el.textContent += `${email.body}`
+        mail_div_element.innerHTML = ""
+        mail_div_element.append(sender_el, recipients_el, subject_el, timestamp_el, body_el);
+
+    });
+
+
+    
 }
